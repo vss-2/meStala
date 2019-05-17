ef6d1a22f6f90233db2f98e312fc903a96bad1b4
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-05-16 21:34:49 -0300

    mainajuda man
    Desisto, vai dar ruim

--- main.sh
+++ main.sh
@@ -1,10 +1,28 @@
-function InstalarOpera{
+#!/bin/bash
+
+function printar {
+	tput clear
+	tput bold
+	printf("$(L)")
+}
+
+function WGET {
+	<(wget "$2" -O "$3" --header "$4" 2>&1)
+}
+
+function DPKG {
+	dpkg --vextract "$2" "$3" | pv -elnps $(dpkg -c "$2" | wc -l) 0 0 0
+}
+
+function InstalarOpera {
 	CCC "Instalando Navegador Opera\n"
 	mkdir -p /tmp/code
 	WGET "Opera Browser" http://deb.opera.com/opera-stable/ /temporario/opera.deb
 	DPKG "Opera Browser" /temporario/opera.deb /temporario/opera
-	CP "Opera Browser" /temporario/opera/usr* $HOME/.local/bin
+	CP "Opera Browser" /temporario/opera/usr/* $HOME/.local/bin
+	printar "Opera instalado com sucesso! "
 }
 
 INIT
-  CCC "Eae man/mana estou torcendo que isso funcione, \n então escolhe uma opção abaixo que eu vou torcer pra rodar\n não tenta trollar senão num funciona :)"
+  printar "Eae man/mana estou torcendo que isso funcione, \n então escolhe uma opção abaixo que eu vou torcer pra rodar\n não tenta trollar senão num funciona :)"
+
